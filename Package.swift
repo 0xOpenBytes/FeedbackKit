aaf5c7be5ae529cd44bca95c974f712fbf385f6e
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2024-09-24 08:48:11 +0300

    upgrade AppState to 2.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/0xLeif/AppState.git", from: "1.0.0")
+        .package(url: "https://github.com/0xLeif/AppState.git", from: "2.0.0")
     ],
     targets: [
         .target(
